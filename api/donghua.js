9fb69385548be4972ad8c1397385cdbae3818dca
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2026-08-20 16:20:29 +0700

    Update donghua.js

--- api/donghua.js
+++ api/donghua.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const targetUrl = `https://animexin.dev/anime/?page=${page}&status=&type=&order=update`;
     const response = await fetch(targetUrl, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
       }
     });
 
